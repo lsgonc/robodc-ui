d05c40d48f74b213c057234cdb47185c1cfbe7e8
Lucas Sciarra <lucasscgoncalves@gmail.com>
2025-06-11 22:13:39 -0300

    export components

--- projects/robodc-ui/src/public-api.ts
+++ projects/robodc-ui/src/public-api.ts
@@ -4,3 +4,12 @@
 
 export * from './lib/robodc-ui.service';
 export * from './lib/robodc-ui.component';
+export * from './lib/alert-modal/alert-modal.component';
+export * from './lib/button/button.component';
+export * from './lib/card/card.component';
+export * from './lib/card-list/card-list.component';
+export * from './lib/detail-modal/detail-modal.component';
+export * from './lib/link/link.component';
+export * from './lib/text/text.component';
+export * from './lib/title/title.component';
+export * from './lib/alert-modal/alert-modal.component';
